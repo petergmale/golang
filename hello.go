586537d2ba5fe0df31a950cc6b967be0c522097d
pgeorge <petergmale@gmail.com>
2015-10-07 20:18:38 -0700

    initial commit of hello.go

---
+++ hello.go
@@ -0,0 +1,12 @@
+
+package main
+
+import (
+ "fmt"
+ "github.com/petergmale/mystring"
+ )
+
+func main() {
+      fmt.Printf( "hello, gopher dude!\n" )
+      fmt.Printf( mystring.Reverse("hello, gopher dude!\n") )
+}
